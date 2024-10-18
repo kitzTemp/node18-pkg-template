2f4ef72c31c6fe136254a1edf1ca4450d8d1036f
Suphakit P <me@suphakit.net>
2024-10-18 09:37:59 +0700

    init<all>: initial commit

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+const yargs = require("yargs");
+
+const module = require("./index");
+
+const usage = ``;
+
+const opt = yargs.usage(usage).argv;
---
+++ bin/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+function mainFunction(filePath) {
+  // Do Function Here
+}
+
+module.exports = mainFunction
